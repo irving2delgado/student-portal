4e8221d7dd5a18e398c86e3dc06d4273e5a63b3b
Irving Delgado <irving2delgado@gmail.com>
2018-05-25 17:55:27 +0000

    additions

--- app.js
+++ app.js
@@ -1,9 +1,29 @@
-var express = require("express");
-var mongoose =require("mongoose");
+var express               = require("express"),
+    mongoose              = require("mongoose"),
+    passport              = require("passport"),
+    User                  = require("./models/user"),
+    bodyParser            = require("body-parser"),
+    LocalStrategy         = require("passport-local"),
+    passportLocalMongoose = require("passport-local-mongoose")
+
 mongoose.connect("mongodb://localhost/student-portal");
 
 var app = express();
 app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(require("express-session")({
+    secret: "bagu wagu",
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+//=================================
+// Routes
 
 app.get("/", function(req, res){
     res.render("home");
@@ -12,6 +32,27 @@ app.get("/", function(req, res){
 app.get("/secret", function(req, res){
     res.render("secret");
 });
+
+// =============Auth routes
+//sign up form
+app.get("/register", function(req, res){
+    res.render("register");
+});
+//handlling user sign up
+app.post("/register", function(req, res){
+    req.body.username;
+    req.body.password;
+    User.register(new User({username: req.body.username}), req.body.password, function(err, user){
+        if(err){
+            console.log(err);
+            return res.render('register');
+        }
+        passport.authenticate("local")(req, res, function(){
+            res.redirect("/secret");
+        });
+    });
+});
+
 
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("sever started");
